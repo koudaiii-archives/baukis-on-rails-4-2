475fe489eeca116c8b996c69c4db97dd4bd726e1
koudaiii <cs006061@gmail.com>
2016-01-11 01:22:26 +0900

    Fix handling layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,10 @@ class ApplicationController < ActionController::Base
 
   private
   def set_layout
-    if params[:controller].match(%r{\A(staff|admin|customer)/)})
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       'customer'
+    end
   end
 end

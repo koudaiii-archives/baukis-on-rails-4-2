91d8f232d6d1f45279e4cbddc91f10fd41a9b935
koudaiii <cs006061@gmail.com>
2016-01-17 18:04:44 +0900

    Change application_controller to concerns/error_handler in error handle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,7 @@ class ApplicationController < ActionController::Base
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  rescue_from Exception, with: :rescue500
-
-  rescue_from Forbidden, with: :rescue403
-  rescue_from IpAddressRejected, with: :rescue403
-  rescue_from ActionController::RoutingError, with: :rescue404
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+  include ErrorHandlers
 
   layout :set_layout
 
@@ -22,23 +17,5 @@ class ApplicationController < ActionController::Base
     else
       'customer'
     end
-  end
-
-  def rescue403(exception = nil)
-    @exception = exception
-    logger.info "Rendering 403 with exception: #{@exception.message}" if @exception
-    render template: 'errors/error_403', status: 403, layout: 'customer', content_type: 'text/html'
-  end
-
-  def rescue404(exception = nil)
-    @exception = exception
-    logger.info "Rendering 404 with exception: #{@exception.message}" if @exception
-    render template: 'errors/error_404', status: 404, layout: 'customer', content_type: 'text/html'
-  end
-
-  def rescue500(exception = nil)
-    @exception = exception
-    logger.info "Rendering 500 with exception: #{@exception.message}" if @exception
-    render template: 'errors/error_500', status: 500, layout: 'customer', content_type: 'text/html'
   end
 end

edae3f9f64bcd82a56599263b31862dca5240719
koudaiii <cs006061@gmail.com>
2016-01-17 16:16:56 +0900

    Add Class IpAddressRejected and Forbidden

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
   rescue_from Exception, with: :rescue500
 
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
   rescue_from ActionController::RoutingError, with: :rescue404
   rescue_from ActiveRecord::RecordNotFound, with: :rescue404
-
 
   layout :set_layout
 

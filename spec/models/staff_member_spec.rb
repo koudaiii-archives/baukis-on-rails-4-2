a35b8777aa396018994f59deaa845f9c2617fc5b
koudaiii <cs006061@gmail.com>
2016-01-22 09:37:15 +0900

    Add spec staff_member

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe StaffMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#password=' do
+    example '文字列を与えると、hashed_passwordは長さ60文字列になる' do
+      member = StaffMember.new
+      member.password = 'baukis'
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to wq(60)
+    end
+  end
 end

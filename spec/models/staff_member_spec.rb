1c5700378ce21a16c3b07c412207d30635abab4a
koudaiii <cs006061@gmail.com>
2016-01-23 19:02:03 +0900

    Add spec. password = nil is hashed_password = nil

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe StaffMember, type: :model do
       expect(member.hashed_password).to be_kind_of(String)
       expect(member.hashed_password.size).to eq(60)
     end
+
+    example 'nil を与えると、 hashed_password は nil になる' do
+      member = StaffMember.new(hashed_password: 'x')
+      member.password = nil
+      expect(member.hashed_password).to be_nil
+    end
   end
 end

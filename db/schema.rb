4233e879c4e6dca7e1422140eed5be531ecd4d66
koudaiii <cs006061@gmail.com>
2016-01-30 17:22:36 +0900

    be rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117131848) do
+ActiveRecord::Schema.define(version: 20160127002624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "administrators", force: :cascade do |t|
+    t.string   "email",                           null: false
+    t.string   "email_for_index",                 null: false
+    t.string   "hashed_password"
+    t.boolean  "suspended",       default: false, null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "administrators", ["email_for_index"], name: "index_administrators_on_email_for_index", unique: true, using: :btree
 
   create_table "staff_members", force: :cascade do |t|
     t.string   "email",                            null: false

4ffc23594d29fdb50b1e0f8676b518f98d90ac14
koudaiii <cs006061@gmail.com>
2016-02-06 18:30:01 +0900

    Add Session#new

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -1,2 +1,10 @@
-class Staff::SessionsController < ApplicationController
+class Staff::SessionsController < Staff::Base
+  def new
+    if current_steaff_member
+      redirect_to :staff_root
+    else
+      @form = Staeff::LoginForm.new
+      render action: 'new'
+    end
+  end
 end

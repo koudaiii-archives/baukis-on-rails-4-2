adce6bd184f2bdb8f58196b49dd531a6359d2ea4
koudaiii <cs006061@gmail.com>
2016-02-07 22:10:02 +0900

    Fix Session#Create

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -11,6 +11,9 @@ class Staff::SessionsController < Staff::Base
   def create
     @form = Staff::LoginForm.new(params[:staff_login_form])
     if @form.email.present?
+      staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
+    end
+    if staff_member
       session[:staff_member_id] = staff_member
       redirect_to :staff_root
     else

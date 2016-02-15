20cdeeb2bd1cf46f9c3097cb22c27bd2667e8e0b
koudaiii <cs006061@gmail.com>
2016-02-15 10:08:08 +0900

    Add staff members in factroy

--- spec/factories/staff_members.rb
+++ spec/factories/staff_members.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
   factory :staff_member do
-      
+    sequence(:email){ |n| "member#{n}@example.com" }
+    family_name  "山田#{n}"
+    given_name  "太郎"
+    family_name_kana 'ヤマダ'
+    given_name_kana 'タロウ'
+    password 'pw'
+    start_date { Date.yesterdasy }
+    end_date nil
+    suspended false
   end
 
 end

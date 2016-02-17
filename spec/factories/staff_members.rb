df19207fbcea78b1b028184a40b0856eff246dee
koudaiii <cs006061@gmail.com>
2016-02-17 21:59:39 +0900

    Fix typo

--- spec/factories/staff_members.rb
+++ spec/factories/staff_members.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :staff_member do
     sequence(:email){ |n| "member#{n}@example.com" }
-    family_name  "山田#{n}"
+    family_name  "山田"
     given_name  "太郎"
     family_name_kana 'ヤマダ'
     given_name_kana 'タロウ'
     password 'pw'
-    start_date { Date.yesterdasy }
+    start_date { Date.yesterday }
     end_date nil
     suspended false
   end

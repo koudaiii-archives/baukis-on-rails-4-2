b30880fdf7540a1ba58b24416ab22e509d6bd969
koudaiii <cs006061@gmail.com>
2016-02-22 16:09:07 +0900

    suspended true user

--- db/seeds/development/staff_members.rb
+++ db/seeds/development/staff_members.rb
@@ -7,3 +7,13 @@ StaffMember.create!(
   password: 'password',
   start_date: Date.today,
 )
+StaffMember.create!(
+  email: 'test@example.com',
+  family_name: '山田',
+  given_name: '太郎',
+  family_name_kana: 'ヤマダ',
+  given_name_kana: 'タロウ',
+  password: 'password',
+  suspended: true,
+  start_date: Date.today,
+)

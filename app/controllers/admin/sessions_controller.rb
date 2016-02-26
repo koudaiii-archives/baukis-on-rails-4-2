326576353ad0e695d3b09cd69fd1e292a4a1c4e3
koudaiii <cs006061@gmail.com>
2016-02-26 11:18:53 +0900

    Add session#create

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -8,4 +8,24 @@ class Admin::SessionsController < ApplicationController
     end
   end
 
+  def create
+    @form = Admin::LoginForm.new(params[:admin_login_form])
+    if @form.email.present?
+      administrator = Administrator.find_by(email_for_index: @form.email.downcase)
+    end
+    authenticator = Admin::Authenticator.new(administrator)
+    if !administrator.nil? && authenticator.authenticate(@form.password)
+      if authenticator.account_lock?
+        flash.now.alert = 'アカウントが停止されています'
+        render action: 'new'
+      else
+        session[:staff_member_id] = administrator.id
+        flash.notice = 'ログインしました。'
+        redirect_to :staff_root
+      end
+    else
+      flash.now.alert = 'メールアドレスまたはパスワードが正しくありません。'
+      render action: 'new'
+    end
+  end
 end

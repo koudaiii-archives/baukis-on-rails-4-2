2850289f8a3d30f565ab5398a0f995cacb5cccfe
koudaiii <cs006061@gmail.com>
2016-02-28 18:56:46 +0900

    only email in Admin login

--- app/services/admin/authenticator.rb
+++ app/services/admin/authenticator.rb
@@ -2,10 +2,12 @@ class Admin::Authenticator
   def initialize(admin_user)
     @admin = admin_user
   end
+
   def authenticate
-    @amdin &&
-      @admin.start_date <= Date.today &&
-      (@admin.end_date.nil? || @admin.end_date > Date.today) &&
-      BCrypt::Password.new(@admin.hashed_password) == raw_password
+    @amdin && @admin.email
+  end
+
+  def account_lock?
+    @amdin.suspended?
   end
 end

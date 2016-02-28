08480ff1bbd86372b04239fc9dacaf356301e4be
koudaiii <cs006061@gmail.com>
2016-02-28 18:19:58 +0900

    Add authenticate

--- app/services/admin/authenticator.rb
+++ app/services/admin/authenticator.rb
@@ -2,4 +2,10 @@ class Admin::Authenticator
   def initialize(admin_user)
     @admin = admin_user
   end
+  def authenticate
+    @amdin &&
+      @admin.start_date <= Date.today &&
+      (@admin.end_date.nil? || @admin.end_date > Date.today) &&
+      BCrypt::Password.new(@admin.hashed_password) == raw_password
+  end
 end

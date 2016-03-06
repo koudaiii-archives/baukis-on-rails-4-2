216d2ef6f8918dd89ef31fc3e515bf75e749746d
koudaiii <cs006061@gmail.com>
2016-03-06 23:35:49 +0900

    Add spec in routing

---
+++ spec/routing/hostname_constraints_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'ルーティング' do
+  example '職員トップページ' do
+    expect(get: 'http://baukis.example.com').to route_to(
+      host: 'baukis.example.com',
+      controller: 'staff/top',
+      action: 'index'
+    )
+  end
+
+  example '管理ログインフォーム' do
+    expect(get: 'http://baukis.example.com/admin/login').to route_to(
+      host: 'baukis.example.com',
+      controller: 'admin/sessions',
+      action: 'new'
+    )
+  end
+end
